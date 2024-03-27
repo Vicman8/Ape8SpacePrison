e0d863768120f14045b72a1139a9bb23432363a6
Vicman8 <Victor2044usa@gmail.com>
2024-03-27 16:11:28 -0400

    Worked on Title Scene (Added play button, how to play, and exit button, added change scene script

---
+++ Assets/Scripts/TitleScreen Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+   public void ToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exited");
+    }
+}
