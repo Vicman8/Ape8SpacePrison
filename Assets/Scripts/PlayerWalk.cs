463b7fafb4da7ce3617a60e4e7d8b72a7befb37c
Vicman8 <Victor2044usa@gmail.com>
2024-03-27 16:00:21 -0400

    Added playwalk script and put it on player in Test scene

--- Assets/Scripts/PlayerWalk.cs
+++ Assets/Scripts/PlayerWalk.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerWalk : MonoBehaviour
 {
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 playerMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        playerMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rb.velocity = playerMove * moveSpeed;
     }
 }
